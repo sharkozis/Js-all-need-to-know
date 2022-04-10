98c4e41f53494e3133dabbbc029c58ed8a95cdd7
sharkozis <hossicoder@gmail.com>
2022-04-10 15:07:42 +0600

    third

--- lesson_11_to_20/app.js
+++ lesson_11_to_20/app.js
@@ -1,11 +1,21 @@
 //les-11-Boolean
 var b3 = Boolean(true)
 var b4 = Boolean(false)
-console.log(b3,b4);
+// console.log(b3,b4);
 
 //les-12-null & undefined
 var abc
 var xyz = null
 
-console.log(abc)
-console.log(xyz)
+// console.log(abc)
+// console.log(xyz)
+
+//les-13-conversion of data type
+var a = '1000';
+var b = 10;
+var c = true;
+// in here we are going to convert string as a number, number as a string and also boolean as a string with constructor function of js. [note that the color of string defined as white in terminal and rest are define as yellow by default] 
+
+console.log(Number(a))
+console.log(b.toString())
+console.log(c.toString())
