8944feaa2c6ada0d76ea553c8198481e309f7cdd
sharkozis <hossicoder@gmail.com>
2022-04-10 17:58:31 +0600

    fourth

--- lesson_11_to_20/app.js
+++ lesson_11_to_20/app.js
@@ -23,5 +23,20 @@ var c = true;
 // les-14-hexadecimal & Octal 
 var hex = 0xff;
 var oct = 0114;
-console.log(hex,oct);
-// [note that to make an hex number we need to add 0x before the number and for octal we will have to 0 before the number]
+// console.log(hex,oct);
+// [note that to make an hex number we need to add 0x before the number and for octal we will have to add 0 before the number]
+
+//les-15-Js Comparison Operator
+var l = 10;
+var h = 20;
+/*
+console.log(l == h) // for match both value
+console.log(l != h) // for not matching both value 
+console.log(l > h ) // for higher value
+console.log(l < h ) // for lower value
+console.log(l >= h) // greater than or equal
+console.log(l <= h) // less than or equal
+console.log(l === h) // for checking both value and types of data are same
+console.log(l !== h) // for checking both value and types of data are not same
+
+*/
