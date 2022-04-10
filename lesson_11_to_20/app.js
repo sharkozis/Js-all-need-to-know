e540065193d2a6deae3388ea829ba5e4a9bfda96
sharkozis <hossicoder@gmail.com>
2022-04-10 15:14:03 +0600

    4th

--- lesson_11_to_20/app.js
+++ lesson_11_to_20/app.js
@@ -16,6 +16,12 @@ var b = 10;
 var c = true;
 // in here we are going to convert string as a number, number as a string and also boolean as a string with constructor function of js. [note that the color of string defined as white in terminal and rest are define as yellow by default] 
 
-console.log(Number(a))
-console.log(b.toString())
-console.log(c.toString())
+// console.log(Number(a));
+// console.log(b.toString());
+// console.log(c.toString());
+
+// les-14-hexadecimal & Octal 
+var hex = 0xff;
+var oct = 0114;
+console.log(hex,oct);
+// [note that to make an hex number we need to add 0x before the number and for octal we will have to 0 before the number]
